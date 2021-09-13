053a99f8c385af2cf10d2451296581a780802d25
atia009 <aarontia009@gmail.com>
2021-09-12 20:49:31 -0700

    Added app.js.  Added navIcon variable and event listener.  Created toggleNav function.

---
+++ app.js
@@ -0,0 +1,12 @@
+// variables
+const navIcon = document.querySelector(".nav-icon");
+
+// functions
+function toggleNav() {
+ console.log(navIcon);
+}
+
+// event listeners
+navIcon.addEventListener("click", function() {
+ toggleNav();
+})
