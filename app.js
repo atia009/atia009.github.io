e7a1949f759c86ab4d8e2316b1f489afd7b4d410
atia009 <aarontia009@gmail.com>
2021-09-14 20:23:37 -0700

    Created nav, social, and category variables.  Modified toggleNav function by adding conditional statement checking for toggleNav class.

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 // variables
+const nav = document.querySelector(".nav");
 const navIcon = document.querySelector(".nav-icon");
+const social = document.querySelector(".social");
+const category = document.querySelector(".category");
 
 // functions
 function toggleNav() {
- console.log(navIcon);
+ if (nav.classList.contains("toggleNav")) {
+  nav.classList.remove("toggleNav");
+  nav.classList.remove("nav-invert");
+  social.classList.add("hidden");
+  category.classList.add("hidden");
+ }
+ else {
+  nav.classList.add("toggleNav");
+  nav.classList.add("nav-invert");
+  social.classList.remove("hidden");
+  category.classList.remove("hidden");
+ }
 }
 
 // event listeners
