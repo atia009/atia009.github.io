59d4b4fe3f5d29ce0736573c80dad9ecf6656a1c
atia009 <aarontia009@gmail.com>
2021-09-16 21:40:50 -0700

    Modified toggleNav functions. Moved event listener to toggleNav.  Added event listener to window. Created categories variable for category__btn class. Created activeNav function and added event listener to categories.

--- app.js
+++ app.js
@@ -3,24 +3,40 @@ const nav = document.querySelector(".nav");
 const navIcon = document.querySelector(".nav-icon");
 const social = document.querySelector(".social");
 const category = document.querySelector(".category");
+const categories = document.querySelectorAll(".category__btn");
 
 // functions
 function toggleNav() {
- if (nav.classList.contains("toggleNav")) {
-  nav.classList.remove("toggleNav");
-  nav.classList.remove("nav-invert");
-  social.classList.add("hidden");
-  category.classList.add("hidden");
- }
- else {
-  nav.classList.add("toggleNav");
-  nav.classList.add("nav-invert");
-  social.classList.remove("hidden");
-  category.classList.remove("hidden");
- }
+ navIcon.addEventListener("click", function() {
+  if (nav.classList.contains("toggleNav")) {
+   nav.classList.remove("toggleNav");
+   nav.classList.remove("nav-invert");
+   social.classList.add("hidden");
+   category.classList.add("hidden");
+  }
+  else {
+   nav.classList.add("toggleNav");
+   nav.classList.add("nav-invert");
+   social.classList.remove("hidden");
+   category.classList.remove("hidden");
+  }
+ })
+}
+
+function activeNav() {
+ categories.forEach(function(category){
+  category.addEventListener("click", function(){
+   categories.forEach(function(category){
+    category.classList.remove("active");
+   })
+   category.classList.add("active");
+  })
+ })
 }
 
 // event listeners
-navIcon.addEventListener("click", function() {
+
+window.addEventListener("DOMContentLoaded", function() {
  toggleNav();
+ activeNav();
 })
